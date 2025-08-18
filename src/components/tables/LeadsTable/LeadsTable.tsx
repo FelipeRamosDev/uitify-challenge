b0ffb5d6e0536cb0d8afcf0bcd3e9370c71ba38d
FelipeRamosDev <felipe@feliperamos.info>
2025-08-18 16:16:51 -0300

    [UIFY-21] Display the lead on the slide
    Introduces SlideOver and DataView reusable components, including styles and types. Integrates SlideOver into LeadsTable to display detailed lead information in a side panel when a row is selected. Also adds a CloseIcon component and updates the display index export.

--- src/components/tables/LeadsTable/LeadsTable.tsx
+++ src/components/tables/LeadsTable/LeadsTable.tsx
@@ -1,10 +1,12 @@
-import { Table } from '@/components/display';
+import { SlideOver, Table } from '@/components/display';
+import DataView from '@/components/display/DataView/DataView';
 import { LeadsFilter } from '@/components/filters';
 import { useFetch } from '@/hooks';
 
 // types
 import type { LeadData } from '@/types/data.types';
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
 export default function LeadsTable(): React.JSX.Element {
    const {
@@ -14,13 +16,12 @@ export default function LeadsTable(): React.JSX.Element {
       error,
       loading
    } = useFetch<LeadData[]>('../../assets/data/leads.json', 'score', 'desc');
-   const [selectedLead, setSelectedLead] = useState<LeadData | null>(null);
+   const [ selectedLead, setSelectedLead ] = useState<LeadData | null>(null);
 
    const handleRowClick = (item: LeadData) => {
       setSelectedLead(item);
    };
 
-   console.log(selectedLead);
    return (
       <div>
          <LeadsFilter defaultData={defaultData} setData={setData} />
@@ -40,6 +41,28 @@ export default function LeadsTable(): React.JSX.Element {
                { key: 'score', label: 'Score' },
             ]}
          />
+
+         {createPortal((
+            <SlideOver
+               isOpen={Boolean(selectedLead)}
+               onClose={() => setSelectedLead(null)}
+            >
+               <div className="slide-header mb-2">
+                  <h2 className="text-lg">Lead Details</h2>
+                  <p className="text-sm">Here you can view and edit lead information.</p>
+               </div>
+
+               <div className="slideBody">
+                  <DataView label="ID" value={selectedLead?.id} />
+                  <DataView label="Name" value={selectedLead?.name} />
+                  <DataView label="Company" value={selectedLead?.company} />
+                  <DataView label="Email" value={selectedLead?.email} />
+                  <DataView label="Status" value={selectedLead?.status} />
+                  <DataView label="Source" value={selectedLead?.source} />
+                  <DataView label="Score" value={selectedLead?.score} />
+               </div>
+            </SlideOver>
+         ), document.querySelector('main#root') as HTMLElement)}
       </div>
    );
 }
